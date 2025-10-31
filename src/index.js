e55fed54f5ea7d03aa59a5d9deda140d3d9e4687
Gia Huy <huyhaibabon861@gmail.com>
2025-10-31 20:49:15 +0700

    seventh commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const productsRoute = require('./routes/products.route');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-
-
+app.use(cors());
 
 app.use('/api', productsRoute);
 
