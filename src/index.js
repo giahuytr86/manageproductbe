f7b64ca6ec55c2264093991080abf03057e82209
Gia Huy <huyhaibabon861@gmail.com>
2025-10-31 20:30:35 +0700

    fourth commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const productsRoute = require('./routes/products.route');
 const app = express();
 app.use(express.json());
 
+
+
 app.use('/api', productsRoute);
 
 const PORT = process.env.PORT || 3000;
@@ -11,3 +13,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
